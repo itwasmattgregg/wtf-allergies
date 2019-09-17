6d64a6f7c45aed3b105b30f3655d37ed0a20373d
Matt Gregg <mattdgregg@gmail.com>
2019-09-17 14:04:31 -0500

    puppeteer core

--- .netlify/functions/get-allergy-info/get-allergy-info.js
+++ .netlify/functions/get-allergy-info/get-allergy-info.js
@@ -1,61 +1,54 @@
 const chromium = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
 
 exports.handler = async (event, context, callback) => {
-  let location = null;
-  let today = null;
-  let tomorrow = null;
+  let theTitle = null
   let browser = null
   console.log('spawning chrome headless')
   try {
-    const executable = await chromium.executablePath
-    browser = await chromium.puppeteer.launch({
+    const executablePath = await chromium.executablePath
+
+    // setup
+    browser = await puppeteer.launch({
       args: chromium.args,
-      executablePath: executable,
-      headless: true,
+      executablePath: executablePath,
+      headless: chromium.headless,
     })
 
-    const zip = JSON.parse(event.body).zip;
+    // Do stuff with headless chrome
+    const page = await browser.newPage()
+    const targetUrl = 'https://davidwells.io'
 
-    const page = await browser.newPage();
+    // Goto page and then do stuff
+    await page.goto(targetUrl, {
+      waitUntil: ["domcontentloaded", "networkidle0"]
+    })
 
-    let responseObj = null;
+    await page.waitForSelector('#phenomic')
 
-    await page.on('response', async (response) => {
-      if (response.request().url().includes('/api/forecast/current/pollen') && response.request().resourceType().startsWith('xhr')) {
-        try {
-          responseObj = await response.json()
-        } catch (err) {
-          console.log(err);
-        }
-      }
-    });
+    theTitle = await page.title();
 
-    await page.goto(`https://www.pollen.com/forecast/current/pollen/${zip}`, { waitUntil: 'networkidle0' });
+    console.log('done on page', theTitle)
 
-    location = await responseObj.Location;
-    today = await location.periods.find(period => period.Type === "Today");
-    tomorrow = await location.periods.find(period => period.Type === "Tomorrow");
-
-  } catch (err) {
-    console.log('error', err)
+  } catch (error) {
+    console.log('error', error)
     return callback(null, {
       statusCode: 500,
       body: JSON.stringify({
-        error: err
+        error: error
       })
-    });
+    })
   } finally {
+    // close browser
     if (browser !== null) {
-      await browser.close();
+      await browser.close()
     }
   }
 
   return callback(null, {
     statusCode: 200,
     body: JSON.stringify({
-      displayLocation: location.DisplayLocation,
-      today,
-      tomorrow,
+      title: theTitle,
     })
   })
-};
+}
